461767665aa62600138c27f72980b94fa2d7afd4
Jock Dalby <jock.dalby@education.govt.nz>
2017-03-15 20:49:52 +1300

    Remove console.log form index route

--- app/routes/restaurants/index.js
+++ app/routes/restaurants/index.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    console.log(this.store.findAll('restaurant'))
     return this.store.findAll('restaurant');
   }
 });
